deff1c5e2355d36d5d9426e0fbd95ca3b2f9d6b1
Dandi Juliandi <dandi.juliandi@nawadata.id>
2024-07-30 14:03:29 +0700

    exclude types.ts file from vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,38 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    setupFiles: ['./vitest.setup.ts'],
-    coverage: {
-      provider: 'v8',
-      reporter: ['text', 'json', 'html'],
-      exclude: [
-        'coverage/**',
-        'dist/**',
-        '**/node_modules/**',
-        '**/[.]**',
-        'packages/*/test?(s)/**',
-        '**/*.d.ts',
-        '**/virtual:*',
-        '**/__x00__*',
-        '**/\x00*',
-        'cypress/**',
-        'test?(s)/**',
-        'test?(-*).?(c|m)[jt]s?(x)',
-        '**/*{.,-}{test,spec,bench,benchmark}?(-d).?(c|m)[jt]s?(x)',
-        '**/__tests__/**',
-        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*',
-        '**/vitest.{workspace,projects}.[jt]s?(on)',
-        '**/.{eslint,mocha,prettier}rc.{?(c|m)js,yml}',
-        '**/src/main.tsx',
-      ]
+    plugins: [react()],
+    test: {
+        environment: 'jsdom',
+        setupFiles: ['./vitest.setup.ts'],
+        coverage: {
+            provider: 'v8',
+            reporter: ['text', 'json', 'html'],
+            exclude: [
+                'coverage/**',
+                'dist/**',
+                '**/node_modules/**',
+                '**/[.]**',
+                'packages/*/test?(s)/**',
+                '**/*.d.ts',
+                '**/virtual:*',
+                '**/__x00__*',
+                '**/\x00*',
+                'cypress/**',
+                'test?(s)/**',
+                'test?(-*).?(c|m)[jt]s?(x)',
+                '**/*{.,-}{test,spec,bench,benchmark}?(-d).?(c|m)[jt]s?(x)',
+                '**/__tests__/**',
+                '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*',
+                '**/vitest.{workspace,projects}.[jt]s?(on)',
+                '**/.{eslint,mocha,prettier}rc.{?(c|m)js,yml}',
+                '**/src/main.tsx',
+                '**src/**/types.ts',
+            ],
+        },
     },
-  },
-})
+});
