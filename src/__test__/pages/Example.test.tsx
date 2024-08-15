51ab1b4e3883b1538ad0f06447412ad96876a9dc
Dandi <dandi.juliandi@nawadata.id>
2024-08-15 19:38:03 +0700

    add Example Page test

---
+++ src/__test__/pages/Example.test.tsx
@@ -0,0 +1,16 @@
+import { expect, it, describe } from 'vitest';
+import { render, screen } from '@testing-library/react';
+
+import Example from '../../pages/Example';
+
+describe('Example Page', () => {
+    it('render component', () => {
+        render(<Example />);
+
+        const content = screen.getByRole('heading', {
+            name: /example page/i,
+        });
+
+        expect(content).toBeInTheDocument();
+    });
+});
