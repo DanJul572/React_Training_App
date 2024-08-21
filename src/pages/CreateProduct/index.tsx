9a32565b9f6bcb93ddc3c6fd2f5d4930f9228241
Dandi Juliandi <dandi.juliandi@nawadata.id>
2024-08-21 13:35:57 +0700

    move button position in CreateProduct page

--- src/pages/CreateProduct/index.tsx
+++ src/pages/CreateProduct/index.tsx
@@ -1,12 +1,13 @@
 import { Controller } from 'react-hook-form';
 
 import Button from '@mui/material/Button';
-
-import { formStyle } from './styles';
+import Box from '@mui/material/Box';
 
 import LabelField from './components/LabelField';
 import ImageField from './components/ImageField';
 import TypeField from './components/TypeField';
+
+import { containerButtonStyle, formStyle } from './styles';
 
 import useCreateProduct from './hooks';
 
@@ -15,45 +16,49 @@ const CreateProduct = () => {
         useCreateProduct();
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)} style={formStyle}>
-            <Controller
-                name="label"
-                control={control}
-                render={({ field, fieldState, formState }) => (
-                    <LabelField
-                        field={field}
-                        resetField={resetField}
-                        fieldState={fieldState}
-                        formState={formState}
-                    />
-                )}
-            />
-            <Controller
-                name="image"
-                control={control}
-                render={({ field, fieldState, formState }) => (
-                    <ImageField
-                        field={field}
-                        resetField={resetField}
-                        fieldState={fieldState}
-                        formState={formState}
-                    />
-                )}
-            />
-            <Controller
-                name="type"
-                control={control}
-                render={({ field, fieldState, formState }) => (
-                    <TypeField
-                        field={field}
-                        resetField={resetField}
-                        fieldState={fieldState}
-                        formState={formState}
-                    />
-                )}
-            />
-            <Button type="submit">Submit</Button>
-        </form>
+        <Box>
+            <form onSubmit={handleSubmit(onSubmit)} style={formStyle}>
+                <Controller
+                    name="label"
+                    control={control}
+                    render={({ field, fieldState, formState }) => (
+                        <LabelField
+                            field={field}
+                            resetField={resetField}
+                            fieldState={fieldState}
+                            formState={formState}
+                        />
+                    )}
+                />
+                <Controller
+                    name="image"
+                    control={control}
+                    render={({ field, fieldState, formState }) => (
+                        <ImageField
+                            field={field}
+                            resetField={resetField}
+                            fieldState={fieldState}
+                            formState={formState}
+                        />
+                    )}
+                />
+                <Controller
+                    name="type"
+                    control={control}
+                    render={({ field, fieldState, formState }) => (
+                        <TypeField
+                            field={field}
+                            resetField={resetField}
+                            fieldState={fieldState}
+                            formState={formState}
+                        />
+                    )}
+                />
+            </form>
+            <Box sx={containerButtonStyle}>
+                <Button type="submit">Submit</Button>
+            </Box>
+        </Box>
     );
 };
 
--- src/pages/CreateProduct/styles.ts
+++ src/pages/CreateProduct/styles.ts
@@ -1,7 +1,15 @@
 import { CSSProperties } from 'react';
 
 export const formStyle: CSSProperties = {
+    marginTop: '20px',
     display: 'flex',
     flexDirection: 'column',
-    gap: '10px',
+    gap: '20px',
+};
+
+export const containerButtonStyle: CSSProperties = {
+    marginTop: '10px',
+    display: 'flex',
+    justifyContent: 'flex-end',
+    backgroundColor: 'red',
 };
