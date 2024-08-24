b1f759742d37a724fea47e002fa8921aef525f33
Dandi <dandi.juliandi@nawadata.id>
2024-08-24 21:59:49 +0700

    init storybook and npm library

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import { resolve } from 'path'
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -39,6 +39,23 @@ export default defineConfig({
                 '**src/types/**',
                 '**src/Routes.tsx',
             ],
+        },
+    },
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'src/main.ts'),
+            name: 'zapp',
+            fileName: 'zapp',
+        },
+        rollupOptions: {
+            external: ["react", "react-dom", "react/jsx-runtime"],
+            output: {
+                globals: {
+                    react: "React",
+                    "react-dom": "ReactDOM",
+                    "react/jsx-runtime": "react/jsx-runtime",
+                },
+            },
         },
     },
 });
