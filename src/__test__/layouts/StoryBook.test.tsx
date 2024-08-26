e9b15b223af7322e638ee8d6bbb0d09796fd885f
Dandi <dandi.juliandi@nawadata.id>
2024-08-26 22:08:34 +0700

    add unit test for StoryBook layout

---
+++ src/__test__/layouts/StoryBook.test.tsx
@@ -0,0 +1,16 @@
+import { expect, it, describe } from 'vitest';
+import { render, screen } from '@testing-library/react';
+
+import StoryBook from '@/layouts/StoryBook';
+
+describe('StoryBook Layout', () => {
+    it('render component', () => {
+        render(
+            <StoryBook>
+                <h1>Hello</h1>
+            </StoryBook>
+        );
+
+        expect(screen.getByText('Hello')).toBeInTheDocument();
+    });
+});
