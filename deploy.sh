1e6579da7a46b280f3138147cb2b282887c6d8a5
Dandi <dandi.juliandi@nawadata.id>
2024-09-27 10:12:28 +0700

    add deployment script

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Navigate to the React Native Training directory
+cd "React_Training_App" || exit
+
+# Pull the latest changes from the main branch
+git pull origin main
+
+# Install dependencies
+yarn install
+
+# Build the project
+yarn build:dev
+
+# Remove all files and folders inside /var/www/192.168.1.10
+sudo rm -rf /var/www/192.168.1.10/*
+
+# Copy all files and folders from dist to /var/www/192.168.1.10
+sudo cp -r dist/* /var/www/192.168.1.10/
+
+# Restart Nginx
+sudo systemctl restart nginx
