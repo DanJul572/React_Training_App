bdae2516006623464a003f5f1fb60bedc1f1da7b
Dandi <dandi.juliandi@nawadata.id>
2025-01-25 20:24:21 +0700

    remove checking access in tree menu

--- src/configs/tree.ts
+++ src/configs/tree.ts
@@ -2,8 +2,6 @@ import translator from '@/helpers/translator';
 
 import { MenuType } from '@/types';
 
-const isAdmin = localStorage.getItem('role_id') === '1';
-
 const tree: MenuType[] = [
     {
         id: '1',
@@ -15,14 +13,11 @@ const tree: MenuType[] = [
         label: translator('product'),
         url: '/product',
     },
-];
-
-if (isAdmin) {
-    tree.push({
+    {
         id: '3',
         label: translator('transaction'),
         url: '/transaction',
-    });
-}
+    },
+];
 
 export default tree;
