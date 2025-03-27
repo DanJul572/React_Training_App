b4c3cb73347abb4d808938adc6b72cf95b8f0508
Dandi Juliandi <dandi.juliandi@nawadata.id>
2025-03-27 15:44:45 +0700

    update library

--- src/pages/Dashboard/components/CardList/index.tsx
+++ src/pages/Dashboard/components/CardList/index.tsx
@@ -1,4 +1,4 @@
-import Grid2 from '@mui/material/Grid2';
+import Grid from '@mui/material/Grid';
 
 import CardItem from '../CardItem';
 
@@ -9,20 +9,20 @@ import translator from '@/helpers/translator';
 const CardList = () => {
   const { dataCount } = useCardList();
   return (
-    <Grid2 container spacing="10px" sx={containerStyle}>
-      <Grid2 size={6}>
+    <Grid container spacing="10px" sx={containerStyle}>
+      <Grid size={6}>
         <CardItem
           title={translator('product')}
           content={dataCount.productCount.toString()}
         />
-      </Grid2>
-      <Grid2 size={6}>
+      </Grid>
+      <Grid size={6}>
         <CardItem
           title={translator('user')}
           content={dataCount.userCount.toString()}
         />
-      </Grid2>
-    </Grid2>
+      </Grid>
+    </Grid>
   );
 };
 
